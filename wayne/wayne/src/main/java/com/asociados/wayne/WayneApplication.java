ef916329cb37e291c1e544636c9717cc8fa8b5f5
Albieri Alaña <aalana@bancolombia.com.co>
2025-10-21 16:13:24 -0400

    Base del backend

---
+++ wayne/wayne/src/main/java/com/asociados/wayne/WayneApplication.java
@@ -0,0 +1,13 @@
+package com.asociados.wayne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WayneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WayneApplication.class, args);
+	}
+
+}
---
+++ wayne/wayne/src/test/java/com/asociados/wayne/WayneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asociados.wayne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WayneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
